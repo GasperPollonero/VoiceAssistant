1e455434edf50343cfa4dc5786b698b392c546be
Prospero Gallone <gasper.pollonero@gmail.com>
2025-12-20 02:25:13 +0100

    Local TTS Server
    The objective is to reduce the latency between the text response and
    voice response of the assistant.
    
    ServerTTS.py is a class with three external methods:
     start():
     	initializes the server, opening piper.exe in background
    	and two threads, one for storing the piper results in a queue
    	and the other to read the results from the queue and playing
    	them.
     end():
     	closes the server, stopping piper and the two threads.
     speak(text):
     	receives the text to be synthetized.

---
+++ ServerTTS.py
@@ -0,0 +1,145 @@
+import subprocess
+import threading
+import queue      # thread-safe list of FIFO type (First In First Out)
+import json
+from pathlib import Path
+
+import numpy as np
+import sounddevice as sd
+
+def load_tts_config() -> dict:
+    """Load and return TTS configuration from JSON file."""
+    
+    base_dir = Path(__file__).resolve().parent
+    config_path = base_dir / "config" / "tts_config.json"
+    with config_path.open("r", encoding="utf-8") as f:
+        return json.load(f)
+
+class PiperTTS:
+    
+    def __init__(self, config_path : str | Path = ""):
+        
+        self.cfg = load_tts_config()
+        self.proc = None
+        self.audio_thread = None
+        self.audio_queue = queue.Queue()
+        
+        self.sample_rate : int = self.cfg["SAMPLE_RATE"]
+        self.channels : int = self.cfg["CHANNELS"]
+        self.block_size : int = self.cfg["BLOCK_SIZE"]
+        
+        self._reader_thread = threading.Thread | None = None
+        self._stop_flag : threading.Event = threading.Event()
+        self._stream : sd.OutputStream | None = None
+        
+        
+    def start(self):
+        """ Runs Piper in persistent mode. """
+        
+        if self.proc is not None:
+            return # already running
+        
+        args = [
+            self.cfg["PIPER_BIN"], 
+            "--model", self.cfg["MODEL_PATH"],
+            "--output-raw",
+            self.cfg["extra_args_server"]
+        ]
+        
+        self.proc = subprocess.Popen(
+            args,
+            stdin  = subprocess.PIPE,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.DEVNULL,
+            bufsize= 0
+        )
+        
+        self._stop_flag.clear()
+        
+        self._reader_thread = threading.Thread(
+            target = self._audio_reader_loop,
+            daemon = True
+        )
+        
+        self._reader_thread.start()
+        
+        self._stream = sd.OutputStream(
+            samplerate = self.sample_rate,
+            channels = self.channels,
+            dtype = "int16",
+            blocksize = self.block_size,
+            callback = self._audio_callback
+        )
+        
+        self.stream.start()
+    
+    def stop(self):
+        """ Stops stream audio and Piper. """
+        
+        self._stop_flag.set()
+        
+        if self._stream is not None:
+            self._stream.stop()
+            self._stream.close()
+            self._stream = None
+            
+        if self.proc is not None:
+            try:
+                self.proc.stdin.close()
+            except Exception:
+                pass
+            self.proc.terminate()
+            self.proc = None
+        
+        
+    def _audio_reader_loop(self):
+        """ Reads from piper.stdout and fills the queue with audio bits. """
+        assert self.proc is not None and self.proc.stdout is not None # same as if (condition) : raise AssesionError
+        
+        bytes_per_sample = 2 * self.channels # int16 = 2 byte
+        chunk_bytes = self.block_size * bytes_per_sample
+        
+        while not self._stop_flag.is_set():
+            raw = self.proc.stdout.read(chunk_bytes)
+            if not raw:
+                break # Piper closed stdout
+            
+        data = np.frombuffer(raw, dtype=np.int16) # converts byte into array
+        
+        if self.channels > 1:
+            data = data.reshape(-1, self.channels)
+            
+            
+        self.audio_queue.put(data) # bloks thread exectuion until the queue has enough space
+        """try:
+            self.audio_queue.put(data, timeout=0.5)
+        except queue.Full:
+            pass"""
+            
+
+    def _audio_callback(self, outdata, frames, time, status):
+        
+        """try:
+            chunk = self.audio_queue.get_nowait()
+        except queue.Empty:
+            # No audio ready -> silence
+            outdata[:] = 0
+            return"""
+        chunk = self.audio_queue.get()
+        
+        # if chunk is shorter fill till its length 
+        if len(chunk) < frames:
+            outdata[:len(chunk)] = chunk.reshape(-1, self.channels)
+            outdata[len(chunk):] = 0
+        else:
+            outdata[:] = chunk[:frames].reshape(-1, self.channels)
+        
+    def speak(self, text : str):
+        """ Sends text to Piper: it generates audio that will end to stream """
+        
+        if not text or self.proc is None or self.proc.stdin is None:
+            return
+        
+        line = (text.strip() + "\n").encode("utf-8")
+        self.proc.stdin.write(line)
+        self.proc.stdin.flush()
