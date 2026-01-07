2ee915dcdc407c0968682cbcb57a10f68110328e
Prospero Gallone <gasper.pollonero@gmail.com>
2026-01-07 14:58:27 +0100

    clean main.py

--- main.py
+++ main.py
@@ -2,28 +2,35 @@ from STT import STT
 import NLP
 import actions
 import TTS
-import time
+from time import perf_counter as time
 
 if __name__ == "__main__":
     
-    start = time.perf_counter()
-    
+    # Creates and starts STT.
     stt = STT()
     stt.initialize_STT()
-    text = stt.get_text_from_mic()
     
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f"Tempo di analisi vocale: {elapsed:.2f} s")
-    
-    print("USER: ", text)
-    
-    nlp_result = NLP.get_response(text)
-    
-    print("ASSISTANT: ", nlp_result.reply_text)
-    TTS.speak(nlp_result.reply_text)
-    actions.handle_action(nlp_result.action, nlp_result.params)
-    
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f"Tempo di esecuzione: {elapsed:.2f} s")
+    while True:
+        
+        command = input("Press to start or insert \'exit\' to stop program.\n")
+        
+        if command == "exit":
+            break
+        
+        # converts speech to text.
+        start = time()
+        text = stt.get_text_from_mic()
+        print(f"Tempo di analisi vocale: {(time() - start):.2f} s")
+        print("USER: ", text)
+        
+        # analyzes text.
+        start = time()
+        nlp_result = NLP.get_response(text)        
+        actions_result = actions.handle_action(nlp_result.action, nlp_result.params)
+        print(f"Tempo di analisi NLP: {(time() - start):.2f} s")
+        
+        if isinstance(actions_result, str):
+            print("ASSISTANT: ", nlp_result.reply_text, actions_result)
+        
+            # synthetizes response.
+            TTS.speak(nlp_result.reply_text + actions_result)
